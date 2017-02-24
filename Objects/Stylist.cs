ab8a8c24f416db3b54c759975dd9cb81f56e922f
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-24 08:44:52 -0800

    create base variables for Stylist class and add getters and setters

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -7,6 +7,29 @@ namespace HairSalonApp
 {
     public class Stylist
     {
-        // Content
+        private string _name;
+        private int _id;
+
+        
+
+
+
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetId(int newId)
+        {
+            _id = newId;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
     }
 }

8498688819e150ee5e81529cb3b9bd5c5ee7459b
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-24 11:12:52 -0800

    begin reworking Stylist methods to use as client methods create failing client database starts empty test

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -7,6 +7,206 @@ namespace HairSalonApp
 {
     public class Client
     {
-        // Content
+        // private string _name;
+        // private int _id;
+        //
+        // public Stylist(string newName, int newId = 0)
+        // {
+        //     _name = newName;
+        //     _id = newId;
+        // }
+        // public override bool Equals(System.Object otherStylist)
+        // {
+        //     // This override will allow Stylist.Equals to test each contained value as an identity
+        //     if(!(otherStylist is Stylist))
+        //     {
+        //         return false;
+        //     }
+        //     else
+        //     {
+        //         Stylist newStylist = (Stylist) otherStylist;
+        //         bool nameIdentity = this.GetName() == newStylist.GetName();
+        //         bool idIdentity = this.GetId() == newStylist.GetId();
+        //         return (nameIdentity && idIdentity);
+        //     }
+        // }
+        //
+        // public override int GetHashCode()
+        // {
+        //     // This override will allow Stylist.GetHashCode to function with the override .Equals
+        //     return this.GetName().GetHashCode();
+        // }
+        //
+        // public static List<Stylist> GetAll()
+        // {
+        //     // Returns a list of all the stylists in the stylist table
+        //     List<Stylist> allStylists = new List<Stylist> {};
+        //
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("SELECT * FROM stylists", conn);
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     while(rdr.Read())
+        //     {
+        //         allStylists.Add(new Stylist(rdr.GetString(1), rdr.GetInt32(0)));
+        //     }
+        //
+        //     DB.CloseSqlConnection(rdr, conn);
+        //
+        //     return allStylists;
+        // }
+        //
+        // public void Save()
+        // {
+        //     // Adds a local Stylist Object to the database, won't save if it's a duplicate stylist
+        //     if (this.IsNewStylist() == -1)
+        //     {
+        //         SqlConnection conn = DB.Connection();
+        //         conn.Open();
+        //
+        //         SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@NewName)", conn);
+        //         cmd.Parameters.Add(new SqlParameter("@NewName", this.GetName()));
+        //
+        //         SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //         while(rdr.Read())
+        //         {
+        //             this.SetId(rdr.GetInt32(0));
+        //         }
+        //         DB.CloseSqlConnection(rdr, conn);
+        //     }
+        // }
+        //
+        // public static Stylist Find(int targetId)
+        // {
+        //     // Looks in the database for a stylist with the given id, returns it as a Stylist Object if found, else returns a Stylist object with null values
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("SELECT * FROM stylists WHERE id=@TargetId;", conn);
+        //     cmd.Parameters.Add(new SqlParameter("@TargetId", targetId));
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     string newName = null;
+        //     int newId = 0;
+        //
+        //     while(rdr.Read())
+        //     {
+        //         newName = rdr.GetString(1);
+        //         newId = rdr.GetInt32(0);
+        //     }
+        //
+        //     Stylist foundStylist = new Stylist(newName, newId);
+        //
+        //     DB.CloseSqlConnection(rdr, conn);
+        //     return foundStylist;
+        // }
+        //
+        // public void Update(string newName)
+        // {
+        //     // Updates a Stylists information in the database and alters the local stylist
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("UPDATE stylists SET name=@NewName OUTPUT INSERTED.name WHERE id=@TargetId", conn);
+        //     cmd.Parameters.Add(new SqlParameter("@NewName", newName));
+        //     cmd.Parameters.Add(new SqlParameter("@TargetId", this.GetId()));
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     while(rdr.Read())
+        //     {
+        //         this.SetName(rdr.GetString(0));
+        //     }
+        //
+        //     DB.CloseSqlConnection(rdr, conn);
+        // }
+        //
+        // public void Delete()
+        // {
+        //     // Delete a stylist from the databas. Currently does nothing to their clients
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("DELETE FROM stylists WHERE id=@TargetId;", conn);
+        //     cmd.Parameters.Add(new SqlParameter("@TargetId", this.GetId()));
+        //     cmd.ExecuteNonQuery();
+        //
+        //     if (conn != null)
+        //     {
+        //         conn.Close();
+        //     }
+        // }
+        //
+        // public static List<Stylist> Search(string targetName)
+        // {
+        //     // Returns a list of Stylists from the database if the name is the same
+        //     List<Stylist> foundStylists = new List<Stylist> {};
+        //
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("SELECT * FROM stylists WHERE name LIKE @TargetName;", conn);
+        //     cmd.Parameters.Add(new SqlParameter("@TargetName", "%" + targetName + "%"));
+        //
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     while(rdr.Read())
+        //     {
+        //         foundStylists.Add(new Stylist(rdr.GetString(1), rdr.GetInt32(0)));
+        //     }
+        //     DB.CloseSqlConnection(rdr, conn);
+        //
+        //     return foundStylists;
+        //
+        // }
+        //
+        // public int IsNewStylist()
+        // {
+        //     // Checks if the stylist already exists in the database. Returns the stylist Id if already exists, else returns -1
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("SELECT * FROM stylists WHERE name=@TargetName;", conn);
+        //     cmd.Parameters.Add(new SqlParameter("@TargetName", this.GetName()));
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     int resultId = -1;
+        //
+        //     if(rdr.Read())
+        //     {
+        //         resultId = rdr.GetInt32(0);
+        //     }
+        //
+        //     DB.CloseSqlConnection(rdr, conn);
+        //     return resultId;
+        // }
+        //
+        //
+        //
+        //
+        // public void SetName(string newName)
+        // {
+        //     _name = newName;
+        // }
+        // public string GetName()
+        // {
+        //     return _name;
+        // }
+        // public void SetId(int newId)
+        // {
+        //     _id = newId;
+        // }
+        // public int GetId()
+        // {
+        //     return _id;
+        // }
+        //
+        // public static void DeleteAll()
+        // {
+        //     // Deletes all stylists in the stylist table
+        //     DB.TableDeleteAll("stylists");
+        // }
     }
 }
--- Tests/ClientTests.cs
+++ Tests/ClientTests.cs
@@ -4,19 +4,188 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 
-// namespace HairSalonApp
-// {
-//     public class ClientTest : IDisposable
-//     {
-//         public ClientTest()
-//         {
-//             DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI";
-//         }
-//
-//         // START Facts
-//         public void Dispose()
-//         {
-//             Stylist.Delete();
-//         }
-//     }
-// }
+namespace HairSalonApp
+{
+    public class ClientTest : IDisposable
+    {
+        public ClientTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI";
+        }
+
+        // START Facts
+
+        [Fact]
+        public void Client_TestDBStartsEmpty_EmptyDB()
+        {
+            // Arrange, Act, Assert
+            Assert.Equal(0, Client.GetAll().Count);
+
+        }
+
+        // [Fact]
+        // public void Client_EqualsIdentityTest_ReturnsTrueForIdenticalObjects()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //
+        //     // Act
+        //     Client secondClient = new Client("Bob");
+        //
+        //     // Assert
+        //     Assert.Equal(firstClient, secondClient);
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_Save_SavesToDatabase()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     List<Client> testList = new List<Client>{firstClient};
+        //
+        //     // Act
+        //     firstClient.Save();
+        //
+        //     // Assert
+        //     Assert.Equal(testList, Client.GetAll());
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_Save_AltersLocalId()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     List<Client> testList = new List<Client>{firstClient};
+        //
+        //     // Act
+        //     firstClient.Save();
+        //
+        //     // Assert
+        //     Assert.Equal(testList, Client.GetAll());
+        // }
+        //
+        // [Fact]
+        // public void Client_FindById_ReturnIdenticalObject()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     firstClient.Save();
+        //
+        //     // Act
+        //     Client foundClient = Client.Find(firstClient.GetId());
+        //
+        //     // Assert
+        //     Assert.Equal(firstClient, foundClient);
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_Update_AlterSavedClient()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     firstClient.Save();
+        //     int targetId = firstClient.GetId();
+        //
+        //     // Act
+        //     firstClient.Update("Robert");
+        //
+        //     // Assert
+        //     Assert.Equal("Robert", Client.Find(targetId).GetName());
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_Delete_RemoveInstanceFromDatabase()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     Client secondClient = new Client("Jenny");
+        //     List<Client> testList = new List<Client>{secondClient};
+        //
+        //     // Act
+        //     firstClient.Save();
+        //     secondClient.Save();
+        //     firstClient.Delete();
+        //
+        //     // Assert
+        //     Assert.Equal(testList, Client.GetAll());
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_Search_ReturnClientWithTargetName()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     Client secondClient = new Client("Jenny");
+        //     List<Client> testList = new List<Client>{secondClient};
+        //
+        //     // Act
+        //     firstClient.Save();
+        //     secondClient.Save();
+        //
+        //     // Assert
+        //     Assert.Equal(testList, Client.Search("Jen"));
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_IsNewClient_ReturnNegOneForTrueOtherwiseId()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     Client secondClient = new Client("Jenny");
+        //     firstClient.Save();
+        //     secondClient.Save();
+        //     Client dupeClient = new Client("Blake");
+        //
+        //     // Act
+        //     int result = dupeClient.IsNewClient();
+        //
+        //     // Assert
+        //     Assert.Equal(-1, result);
+        //
+        // }
+        //
+        // [Fact]
+        // public void Client_Save_NoSaveOnDuplicate()
+        // {
+        //     // Arrange
+        //     Client firstClient = new Client("Bob");
+        //     Client secondClient = new Client("Jenny");
+        //     firstClient.Save();
+        //     secondClient.Save();
+        //     Client dupeClient = new Client("Bob");
+        //
+        //
+        //     List<Client> testList = new List<Client>{firstClient, secondClient};
+        //
+        //     // Act
+        //     dupeClient.Save();
+        //
+        //     // Assert
+        //     Assert.Equal(2, Client.GetAll().Count);
+        //
+        // }
+        //
+        //
+        // [Fact]
+        // public void TEST1()
+        // {
+        //     // Arrange
+        //
+        //     // Act
+        //
+        //     // Assert
+        //
+        // }
+
+        public void Dispose()
+        {
+            Client.Delete();
+        }
+    }
+}

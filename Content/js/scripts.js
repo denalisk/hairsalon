f0af341c59d0d3f1f1dccd34b21cf24979628aa7
Sam Kirsch <samdkirsch2@gmail.com>
2017-02-24 15:39:16 -0800

    more styling

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -1,11 +1,12 @@
-$(function() {
-    $(".stylist-form").hide();
-
-    $("#add-stylist").click(function() {
-        $(".stylist-form").show();
-    });
-
-    $("#submit-stylist").click(function() {
-        $(".stylist-form").hide();
-    })
-});
+// $(function() {
+//     alert("locked");
+//     $(".stylist-form").hide();
+//
+//     $("#add-stylist").click(function() {
+//         $(".stylist-form").show();
+//     });
+//
+//     $("#submit-stylist").click(function() {
+//         $(".stylist-form").hide();
+//     })
+// });
